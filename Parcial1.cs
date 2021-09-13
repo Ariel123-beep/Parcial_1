fcd3d6687b9597ad1b9fd074a3c9489d912294d5
Ariel123-beep <ariel1234alas@outlook.com>
2021-09-13 17:28:30 -0600

    Parcial 1

---
+++ Parcial1.cs
@@ -0,0 +1,28 @@
+﻿using Parcial_1.Entidades;
+using Parcial_1.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsUsuario usuario = new ClsUsuario();
+            NclsUsuario nclsusuario = new NclsUsuario();
+
+            Console.WriteLine("Ingresa tu nombre");
+            usuario.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu contraseña");
+            usuario.Contraseña = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(nclsusuario.ACCESO(usuario));
+            Console.ReadLine();
+
+        }
+    }
+}
